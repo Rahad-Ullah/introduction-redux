47855f41f622942c7b2ada9bea911421032d1a6c
Rahad-Ullah <myemail.abc20@gmail.com>
2024-07-02 16:57:49 +0600

    Redux implemented in Todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/CounterSlice";
 
 const App = () => {
-    return (
-        <div>
-            <h1 className="text-3xl">React with Redux</h1>
-        </div>
-    );
+    const {count} = useSelector((state) => state.counter)
+    const dispatch = useDispatch()
+    
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex gap-10 items-center bg-slate-50 p-10 border border-purple-200 rounded-xl">
+        <button onClick={() => dispatch(decrement())} className="btn btn-secondary text-xl">Decrement</button>
+        <span className=" text-4xl">{count}</span>
+        <button onClick={() => dispatch(increment())} className="btn btn-success text-xl">Increment</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
